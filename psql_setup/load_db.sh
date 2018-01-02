f023bc14038b2766857a9ab8201b6e698435d85c
Matteo Hertel <info@matteohertel.com>
2018-01-02 16:16:22 +0000

    Dockerise all the things

--- psql_setup/load_db.sh
+++ psql_setup/load_db.sh
@@ -1,4 +1,4 @@
 #!bin/bash
-wget https://cdn.patricktriest.com/atlas-of-thrones/atlas_of_thrones.sql
-psql -f db_init.sql
-psql atlas_of_thrones < atlas_of_thrones.sql
+#curl -lsfO https://cdn.patricktriest.com/atlas-of-thrones/atlas_of_thrones.sql
+#psql -f db_init.sql
+psql atlas_of_thrones < ./atlas_of_thrones.sql
