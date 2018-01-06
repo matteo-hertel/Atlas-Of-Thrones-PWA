ac7aad889db941024e823492643387079253bf62
Matteo Hertel <info@matteohertel.com>
2018-01-06 15:29:25 +0000

    add a sensible (not perfect) dockerisation

--- psql_setup/load_db.sh
+++ psql_setup/load_db.sh
@@ -1,4 +1,6 @@
-#!bin/bash
-#curl -lsfO https://cdn.patricktriest.com/atlas-of-thrones/atlas_of_thrones.sql
-#psql -f db_init.sql
-psql atlas_of_thrones < ./atlas_of_thrones.sql
+curl -lsfO https://cdn.patricktriest.com/atlas-of-thrones/atlas_of_thrones.sql
+
+docker exec -i -w /tmp/data -t $(docker ps -aqf "name=db_1") bash
+# run `psql -d atlas_of_thrones < ./atlas_of_thrones.sql`
+
+rm atlas_of_thrones.sql
